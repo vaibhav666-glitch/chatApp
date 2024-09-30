764d5a1e412fae271ded3b2148a6395bb61ecc65
vaibhav <vaibhavning@gmail.com>
2024-10-01 03:37:54 +0530

    fix bug

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import cors from 'cors'
+import { Server } from 'socket.io'
+import http from 'http';
+
+
+const app=express();
+
+
+//1. Create server using HttP
+const server=http.createServer(app);
+
+
+//2. Create socket server.
+const io=new Server(server,
+   { cors:{
+        origin:'*',
+        methods:['GET','POST'],
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log('Client connected');
+    socket.on('join',(data)=>{
+        socket.userName=data;
+    })
+    socket.on('message',(message)=>{
+        let userMessage={
+            userName:socket.userName,
+            message:message
+        }
+        console.log(userMessage);
+        //Broadcast message to all connected clients
+        socket.broadcast.emit('broadcastMessage',userMessage);
+    })
+    socket.on('disconnect',()=>{
+        console.log('Client disconnected');
+    })
+})
+
+
+server.listen(3200,()=>{
+    console.log('Server is running on port 3200')
+})
